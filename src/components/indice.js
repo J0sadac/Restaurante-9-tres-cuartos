9bc4d8fe149a00417c7cf35c71c843de38c251aa
Josadac Castaneda <jugdmentc12@gmail.com>
2024-08-13 18:13:56 -0600

    version 0.1 terminada

--- src/components/indice.js
+++ src/components/indice.js
@@ -1,25 +1,31 @@
+import React from 'react';
 import pergamino from '../multimedia/pergamino.png';
 
-function Indice () {
+function Indice({ scrollToSection, refs }) {
 
     return(
         <>
-            <section className="indice">
+            <section className="indice" onClick={() => scrollToSection(refs.snacksRef)}>
                 <img className="pergamino" src={pergamino} alt="indice" />
                 <span className='nombre'>Snacks</span>
             </section>
 
-            <section className="indice">
+            <section className="indice" onClick={() => scrollToSection(refs.platoFuerteRef)}>
                 <img className="pergamino" src={pergamino} alt="indice" />
                 <span className='nombre'>Platos fuertes</span>
             </section>
 
-            <section className="indice">
+            <section className="indice" onClick={() => scrollToSection(refs.bebidasRef)}>
                 <img className="pergamino" src={pergamino} alt="indice" />
                 <span className='nombre'>Bebidas</span>
             </section>
+
+            <section className="indice" onClick={() => scrollToSection(refs.paquetesRef)}>
+                <img className="pergamino" src={pergamino} alt="indice" />
+                <span className='nombre'>Promociones</span>
+            </section>
         </>
     );
-};
+}
 
-export default Indice
+export default Indice;
--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,11 @@
+import React, {useRef} from 'react';
+
 import Logo from '../multimedia/logo.png';
 import gifLogo from '../multimedia/gif restaurante.gif';
+import SeparadorSnacks from '../multimedia/separador snacks.png';
+import SeparadorPlato from '../multimedia/separador plato fuerte.png';
+import SeparadorBebidas from '../multimedia/separador bebidas.png';
+import SeparadorPromos from '../multimedia/separador promociones.png';
 
 import Pergamino from './indice';
 import Snacks from './snacks';
@@ -9,6 +15,14 @@ import Paquetes from './paquetes';
 
 
 function Main () {
+    const snacksRef = useRef(null);
+    const platoFuerteRef = useRef(null);
+    const bebidasRef = useRef(null);
+    const paquetesRef = useRef(null);
+
+    const scrollToSection = (ref) => {
+        ref.current.scrollIntoView({ behavior: 'smooth' });
+    };
 
     return(
         <>
@@ -18,13 +32,30 @@ function Main () {
                 <img className='logo-titulo' src={gifLogo} alt='titulo' />
             </section>
 
-            <Pergamino />
-            <Snacks />
-            <PlatoFuerte />
-            <Bebidas />
-            <Paquetes />
+            <Pergamino scrollToSection={scrollToSection} refs={{snacksRef, platoFuerteRef, bebidasRef, paquetesRef}} />
+            
+
+            <section ref={snacksRef} className='seccion'>
+                <img className='separador' alt='separador' src={SeparadorSnacks} />
+                <Snacks />
+            </section>
+
+            <section ref={platoFuerteRef} className='seccion'>
+                <img className='separador' alt='separador' src={SeparadorPlato} />
+                <PlatoFuerte />
+            </section>
+
+            <section ref={bebidasRef} className='seccion'>
+                <img className='separador' alt='separador' src={SeparadorBebidas} />
+                <Bebidas />
+            </section>
+
+            <section ref={paquetesRef} className='seccion'>
+                <img className='separador' alt='separador' src={SeparadorPromos} />
+                <Paquetes />
+            </section>
         </>
     );
 };
 
-export default Main
+export default Main;
